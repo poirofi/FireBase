d219b6ce21992ea3fe0dae17e8adf35818d2ecce
poirof <poirofirdaus@gmail.com>
2021-10-04 03:03:31 +0700

    FireBase

---
+++ Incremental Game FireBase/Assets/Scripts/LoadingController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingController : MonoBehaviour
+{
+    private void Start()
+
+    {
+
+        UserDataManager.Load();
+
+        SceneManager.LoadScene(1);
+
+    }
+}
---
+++ Incremental Game FireBase/Assets/Scripts/UserProgressData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserProgressData : MonoBehaviour
+{
+    public double Gold = 0;
+
+    public List<int> ResourcesLevels = new List<int>();
+
+}
